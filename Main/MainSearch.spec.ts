6281afa8410459b477cf6780563d377355cfbe45
hassanjaber7 <hassanjaber7@gmail.com>
2026-07-12 11:03:16 +0200

    Adding Rooms Search

--- Main/MainSearch.spec.ts
+++ Main/MainSearch.spec.ts
@@ -7,7 +7,8 @@ test('Searching for apartments in Warsaw', async ({ page }) => {
   test.setTimeout(240000);
   const listingData = []; // Array to store the listing data to push it later to a JSON file
     // Load existing listings
-    const existingListings = await loadExistingListings('listings.json');
+  
+  const existingListings = await loadExistingListings('listings.json');
   await page.goto('https://www.olx.pl/nieruchomosci/mieszkania/wynajem/warszawa/?search%5Bdist%5D=30&search%5Border%5D=created_at:desc&search%5Bfilter_float_price:from%5D=1000&search%5Bfilter_float_price:to%5D=2500');
   
   // await page.waitForLoadState('networkidle');
@@ -65,5 +66,63 @@ test('Searching for apartments in Warsaw', async ({ page }) => {
   console.table(listingData);
 
   await sendListingsToTelegram();
+
+// Searching for rooms in Warsaw
+  await page.goto('https://www.olx.pl/nieruchomosci/stancje-pokoje/warszawa/q-room-for-rent/?search%5Bdist%5D=15&search%5Border%5D=created_at:desc&search%5Bfilter_float_price:from%5D=1000&search%5Bfilter_float_price:to%5D=2200');
+  
+  const CookiesPopupRooms = await page.getByRole('button', { name: 'Akceptuj wszystkie'});
+  if (await CookiesPopupRooms.isVisible()){
+      await CookiesPopupRooms.click();
+  }
+  
+  const listingsRooms = await page.locator('[data-testid="l-card"]').all();
+  console.log(listingsRooms.length);
+  for (const listingRoom of listingsRooms) {
+    const title = await listingRoom.locator('[data-nx-name="H4"]').textContent();
+    const price = await listingRoom.locator('[data-testid="ad-price"]').textContent();
+    const locationDate = await listingRoom.locator('[data-testid="location-date"]').textContent();
+    const link = await listingRoom.locator('[data-testid="card-title-link"]').getAttribute('href');
+    const baseUrl = 'https://www.olx.pl/';
+    const fullLink = baseUrl + link;
+    
+    // Create a unique key for the listing to check for duplicates
+    const listingKey = {
+      title: title?.trim() ?? '',
+      price: price?.trim() ?? '',
+      locationDate: locationDate?.trim() ?? ''
+    };
+   // Check if the listing already exists in the existing listings
+    if (await listingExists(existingListings, listingKey)) {
+      continue;
+    }
+
+    if (locationDate?.includes('dzisiaj')) {
+    // Push the new listing data to the array
+      listingData.push({
+        title: title?.trim() ?? '',
+        price: price?.trim() ?? '',
+        locationDate: locationDate?.trim() ?? '',
+        link: fullLink ?? ''
+      });
+  }
+}
+// Merge the new listings with existing ones and save to JSON
+ const allDataRooms = [...existingListings, ...listingData];
+ fs.writeFileSync('listings.json', JSON.stringify(allDataRooms, null, 2));
+  
+  console.log('💾 Saved to listings.json');
+  // Save the new listings to a separate JSON file for Telegram
+  const jsonDataRooms = JSON.stringify(listingData, null, 2);
+  fs.writeFileSync('MostRecentListings.json', jsonDataRooms);
+
+  // Preview the data
+  console.log('\n📦 First listing:');
+  console.log(listingData[0]);
+  
+  // View all data as table
+  console.table(listingData);
+
+  await sendListingsToTelegram();
+
 });
 
